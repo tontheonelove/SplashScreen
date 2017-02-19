3778e923949bbb6c404d0bb0f929af0d9fcd0e8a
Ton Theonelove <tonlove5233@gmail.com>
2017-02-19 14:38:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tstp/splashscreen/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tstp.splashscreen;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
